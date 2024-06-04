c4dbed106cf7bb877fe8dbe330eac14b57205cc4
VVooJIN3 <131952511+VVooJIN3@users.noreply.github.com>
2024-06-04 11:00:52 +0900

    [Silver IV] Title: 덱 2, Time: 1112 ms, Memory: 70676 KB -BaekjoonHub

---
+++ 백준/Silver/28279. 덱 2/덱 2.py
@@ -0,0 +1,27 @@
+from collections import deque
+import sys
+input=sys.stdin.readline
+N = int(input())
+deq = deque()
+
+for _ in range(N):
+    order = input()
+    if order[0] in ('1', '2'):
+        X = int(order[2:])
+        if order[0] == '1':
+            deq.appendleft(X)
+        elif order[0] == '2':
+            deq.append(X)
+    elif order[0] == '3':
+        print(deq.popleft()) if deq else print(-1)
+    elif order[0] == '4':
+        print(deq.pop()) if deq else print(-1)
+    elif order[0] == '5':
+        print(len(deq))
+    elif order[0] == '6':
+        print(0) if deq else print(1)
+    elif order[0] == '7':
+        print(deq[0]) if deq else print(-1)
+    elif order[0] == '8':
+        print(deq[-1]) if deq else print(-1)
+
